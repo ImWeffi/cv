80a5b88c36fba63792a6e85f01e338ac224cb945
ImWeffi <nikita.karpovs2004@inbox.lv>
2024-05-12 18:03:55 +0300

    Add header

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
 
 function MainPage() {
   return (
     <>
+    <Header/>
     <div>
         main page
     </div>
+    <Footer/>
     </>
   )
 }
